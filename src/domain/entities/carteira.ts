da9162630d9c1b46ff94c835075853c06fc71d13
Lucas Giusti <giusti.lucas@gmail.com>
2025-07-26 16:35:11 -0300

    alterando edição da carteira para permitir usuários user alterar

--- src/domain/entities/carteira.ts
+++ src/domain/entities/carteira.ts
@@ -40,11 +40,6 @@ export class Carteira {
 
     // Métodos de domínio
     update(updatedBy: User, nome: string): void {
-        // Regra 1: Usuários USER não podem alterar carteiras
-        if (updatedBy.userRole === UserRole.USER) {
-        throw new UnauthorizedException('unauthorized.user.cannot.update.carteira');
-        }
-    
         if (nome !== undefined && nome !== null && nome.trim() !== '') {
         this._nome = nome;
         }
