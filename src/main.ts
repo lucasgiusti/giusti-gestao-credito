4ed4aa58d5e8a483cb45a2dbdcd3371c903d0ed7
Lucas Giusti <giusti.lucas@gmail.com>
2025-07-30 13:47:10 -0300

    add whitelist on dtos

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
   }));
   app.useGlobalFilters(new CustomExceptionFilter());
   
